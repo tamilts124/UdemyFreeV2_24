09259d292bf19139187123afa6d99a31778e9aaf
Tamilts124 <Tamilts124@gmail.com>
2024-12-10 02:15:39 +0530

    integration test 8.

--- Modules/UdemyChromeLogin.py
+++ Modules/UdemyChromeLogin.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+driver =null
 
 def launch_form(email: str)->None:
+    global driver
     driver = uc.Chrome()
     driver.get(r'https://www.udemy.com/join/passwordless-auth/?locale=en_US&next=https%3A%2F%2Fwww.udemy.com%2F&response_type=html')
     sleep(5)
